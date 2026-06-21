dd7656ba9b3e432635594f8003efb5d83f086a5c
Ganesh Kumar <ganeshkumar14422@gmail.com>
2026-06-21 20:15:05 +0530

    fix url

--- src/api/request.js
+++ src/api/request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import router from "../router";
 
 // ─── Constants
-// const BASE_URL = "/api";
-const BASE_URL = "https://backend-oms.vercel.app/api/";
+const BASE_URL = "/api";
+// const BASE_URL = "https://backend-oms.vercel.app/api/";
 // const BASE_URL = "http://localhost:3000/api/";
 // const BASE_URL = "https://5r2rd59c-3000.inc1.devtunnels.ms/api/";
 const DEFAULT_TIMEOUT = 30_000;
